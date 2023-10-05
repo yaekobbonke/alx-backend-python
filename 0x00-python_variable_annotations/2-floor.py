485cff0d071f45cda91a431a7b3836acc4846afd
yaekobbonke <yaekobbundure@gmail.com>
2023-10-05 23:03:51 +0300

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import math
+'''floor function'''
+
+
+def floor(n: float) -> float:
+    '''returns floor of n'''
+    floor_value = math.floor(n)
+    return floor_value
