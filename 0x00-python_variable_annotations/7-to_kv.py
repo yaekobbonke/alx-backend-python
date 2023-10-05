27abfc3a655414693f1aba2fcd3e54d3288237b1
yaekobbonke <yaekobbundure@gmail.com>
2023-10-06 00:42:40 +0300

    pycodestyle fix

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,11 +5,12 @@ from typing import List, Union, Tuple
 '''a type-annotated function to_kv
 takes a string k and an int OR float v as arguments
 returns a tuple. The first element of the tuple is the string k.
-The second element is the square of the int/float v 
+The second element is the square of the int/float v
 and should be annotated as a float.'''
 
 
 from typing import Union, Tuple
+
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Return a tuple with string k and the square of int/float v."""
