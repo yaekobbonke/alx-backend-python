46b88cca3e9f6cfa1f05b4f27e8900e7c24a1565
yaekobbonke <yaekobbundure@gmail.com>
2023-10-05 22:54:12 +0300

    basic annotation concat error fixed

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -5,5 +5,5 @@
 
 def concat(str1: str, str2: str) -> str:
     '''concatenates two strings'''
-    concatenated_string = 'str1' + 'str2'
+    concatenated_string = str1 + str2
     return concatenated_string
