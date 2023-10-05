8e430bb9f9a8f55b29bd825edb8ef40bbddf7009
yaekobbonke <yaekobbundure@gmail.com>
2023-10-05 22:50:43 +0300

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+'''concatenation'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''concatenates two strings'''
+    concatenated_string = 'str1' + 'str2'
+    return concatenated_string
