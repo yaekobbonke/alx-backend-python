2a45ed37f653cdc67feef76dc59c268e65a9251d
yaekobbonke <yaekobbundure.gmail.com>
2023-10-29 15:33:02 +0300

    assertraise

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -30,5 +30,29 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
+         @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """
+        Test that a KeyError is raised with the expected error message.
+
+        Parameters:
+        - nested_map (Mapping): A nested map.
+        - path (Sequence): A sequence of keys representing a path to the value.
+
+        Returns:
+        - None
+
+        Raises:
+        - AssertionError: If the expected KeyError is not raised or the error message is incorrect.
+        """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+
+        expected_error_message = f"Key not found: {path[-1]}"
+        self.assertEqual(str(context.exception), expected_error_message)
+
 if __name__ == '__main__':
     unittest.main()
